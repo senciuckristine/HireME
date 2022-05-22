061d54ca60486955a2c84a4bdfce69250ecf09bb
senciuckristine <paula.senciuc@student.upt.ro>
2022-05-23 00:57:55 +0300

    Frontend finished

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShowAdminsList from "./components/ShowAdminsList";
 import Logout from "./components/logout";
 import CreateSurvey from "./components/createSurvey";
 import TakeSurvey from "./components/TakeSurvey";
+import EditProfile from "./components/EditProfile";
 
 const App = () =>(
   <BrowserRouter>
@@ -20,6 +21,7 @@ const App = () =>(
      <Route  path="/logout" element={<Logout/>} />
      <Route path="/createSurvey" element={<CreateSurvey/>}/>
      <Route path="/takeSurvey" element={<TakeSurvey/>}/>
+     <Route path="/editProfile" element={<EditProfile/>}/>
    </Routes >
   </BrowserRouter>
  
